482bb1c147a4557076ccfde40d7b0c9c4be6dde3
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-06-30 17:59:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+   
+   
+     <Navbar  title="TeaxtUtils"  aboutText="About"   ContactText=" Contact"/>
+    </>
+  );
+}
+
+export default App;
